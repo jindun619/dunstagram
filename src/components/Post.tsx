58c3770bbccffb58fb9de8de0c4cf71a338f8303
jindun619 <jindun619@naver.com>
2024-08-15 14:32:30 +0900

    feat: Add lazy loading for images in Post component

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,11 +1,8 @@
 import styled from 'styled-components/native';
 import Icon from 'react-native-vector-icons/Ionicons';
-import {FlatList as RNFlatList} from 'react-native';
-import {Dimensions} from 'react-native';
 import Actions from './Post/Actions';
 import Body from './Post/Body';
-
-const {width: SCREEN_WIDTH} = Dimensions.get('window');
+import ImagesSlider from './Post/ImagesSlider';
 
 const Container = styled.View`
   width: 100%;
@@ -32,16 +29,13 @@ const Id = styled.Text`
   font-size: 15px;
   font-weight: 500;
 `;
-const ImagesSlider = styled.FlatList`` as unknown as typeof RNFlatList;
-const Image = styled.Image`
-  width: ${SCREEN_WIDTH}px;
-  height: ${SCREEN_WIDTH}px;
-`;
 
 const Post = () => {
-  const renderItem = ({item}: {item: string}) => (
-    <Image source={{uri: item}} resizeMode="contain" />
-  );
+  const images = [
+    'https://media.gq.com/photos/63c045b287b2608f9262ff28/16:9/w_1280,c_limit/halland-16x9.jpg',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS10UHlxK5SIpX6uRnBpHscu6EBLe0sUKxhSA&s',
+    'https://www.mancity.com/features/phil-foden-one-of-our-own/assets/VNxkiCqskG/phil-foden-one-of-our-own-750x422.jpg',
+  ];
 
   return (
     <Container>
@@ -56,18 +50,7 @@ const Post = () => {
         </Profile>
         <Icon name="ellipsis-horizontal" size={20} />
       </Header>
-      <ImagesSlider
-        data={[
-          'https://media.gq.com/photos/63c045b287b2608f9262ff28/16:9/w_1280,c_limit/halland-16x9.jpg',
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS10UHlxK5SIpX6uRnBpHscu6EBLe0sUKxhSA&s',
-          'https://www.mancity.com/features/phil-foden-one-of-our-own/assets/VNxkiCqskG/phil-foden-one-of-our-own-750x422.jpg',
-        ]}
-        renderItem={renderItem}
-        keyExtractor={item => item}
-        horizontal
-        pagingEnabled
-        showsHorizontalScrollIndicator={false}
-      />
+      <ImagesSlider images={images} />
       <Actions />
       <Body />
     </Container>
---
+++ src/components/Post/ImagesSlider.tsx
@@ -0,0 +1,67 @@
+import styled from 'styled-components/native';
+import {Dimensions, FlatList as RNFlatList, ViewToken} from 'react-native';
+import {useRef, useState} from 'react';
+
+const {width: SCREEN_WIDTH} = Dimensions.get('window');
+
+const Container = styled.View``;
+const FlatList = styled.FlatList`` as unknown as typeof RNFlatList;
+const Image = styled.Image`
+  width: ${SCREEN_WIDTH}px;
+  height: ${SCREEN_WIDTH}px;
+`;
+const PaginationContainer = styled.View`
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 6px 0;
+`;
+const PaginationDot = styled.View<{active: boolean}>`
+  width: 6px;
+  height: 6px;
+  border-radius: 3px;
+  margin: 0 2px;
+  background-color: ${props => (props.active ? '#3897f0' : '#c7c7c7')};
+`;
+
+interface ImagesSliderProps {
+  images: string[];
+}
+const ImagesSlider = ({images}: ImagesSliderProps) => {
+  const [activeIndex, setImageIndex] = useState(0);
+
+  const renderItem = ({item}: {item: string}) => (
+    <Image source={{uri: item}} resizeMode="contain" />
+  );
+  const renderPagination = () => (
+    <PaginationContainer>
+      {images.map((_, index) => (
+        <PaginationDot key={index} active={index === activeIndex} />
+      ))}
+    </PaginationContainer>
+  );
+
+  const onViewableItemsChanged = useRef(
+    ({viewableItems}: {viewableItems: ViewToken[]}) => {
+      const {index} = viewableItems[0];
+      setImageIndex(index as number);
+    },
+  ).current;
+
+  return (
+    <Container>
+      <FlatList
+        data={images}
+        renderItem={renderItem}
+        keyExtractor={item => item}
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        onViewableItemsChanged={onViewableItemsChanged}
+      />
+      {renderPagination()}
+    </Container>
+  );
+};
+
+export default ImagesSlider;
